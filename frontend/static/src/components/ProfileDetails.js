96a568afdaefa96369a5550cb69df154d51596c8
CalebCraigo <CalebDOS1@yahoo.com>
2019-10-30 17:17:14 -0400

    add log out function

--- frontend/static/src/components/ProfileDetails.js
+++ frontend/static/src/components/ProfileDetails.js
@@ -2,7 +2,9 @@ import React, {Component} from 'react';
 import axios from 'axios';
 import '../Aside.css';
 
-axios.defaults.headers.common['Authorization'] = `Token ${JSON.parse(localStorage.getItem('my-app-user')).token}`;
+if(localStorage.getItem('my-app-user')) {
+   axios.defaults.headers.common["Authorization"] = `Token ${JSON.parse(localStorage.getItem('my-app-user')).token}`;
+}
 
 class ProfileDetails extends Component {
   constructor(props) {
@@ -27,13 +29,24 @@ class ProfileDetails extends Component {
     });
   }
 
+  logout () {
+    axios.post('/api/v1/rest-auth/logout/')
+      .then(res => {
+        localStorage.clear()
+        this.history.push('/');
+      })
+      .catch(error =>{
+        console.log(error);
+      });
+  }
+
   render(){
     return (
       <section className='aside-content'>
         <h1>{this.state.profile.name}</h1>
         <img src={this.state.profile.avatar} alt='' />
         <p>Prefences Place Holder</p>
-
+        <button onClick={this.logout}>Log Out</button>
       </section>
     );
   };
