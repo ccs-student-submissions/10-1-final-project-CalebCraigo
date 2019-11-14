056eb934a60fb781bd7cc3535affe5ef4578ef65
CalebCraigo <CalebDOS1@yahoo.com>
2019-11-14 11:06:40 -0500

    updated profile urls

--- frontend/urls.py
+++ frontend/urls.py
@@ -5,7 +5,7 @@ from .views import IndexView
 app_name = 'frontend'
 
 urlpatterns = [
-    path('profile/<int:pk>', IndexView.as_view(), name='profile-details'),
+    path('profile/', IndexView.as_view(), name='profile-details'),
     path('profile/create/', IndexView.as_view(), name='index'),
     path('signup/', IndexView.as_view(), name='index'),
     path('login/', IndexView.as_view(), name='index'),
